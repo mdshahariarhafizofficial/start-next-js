86b6d5b4547ec1a93e82fd817f153cf2d3ecfe9c
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-03 23:05:32 +0600

    Feature Section Added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./Components/Hero";
+import Feature from "./Components/Feature";
 
 export default function Home() {
   return (
     <>
       <Hero></Hero>
+      <Feature></Feature>
     </>
   );
 }
