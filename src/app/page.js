8b6adca6ecf1fab4dabe2183673a4b7a3a1167a9
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-04 22:30:35 +0600

    Blog Section Added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./Components/Hero";
 import Feature from "./Components/Feature";
+import Blogs from "./Components/Blogs";
 
 export default function Home() {
   return (
     <>
       <Hero></Hero>
       <Feature></Feature>
+      <Blogs></Blogs>
     </>
   );
 }
