55b28417f5474c06d7a494444cf5f9564f66954a
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-05 11:41:36 +0600

    Loading and error page added

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+<div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+    );
+};
+
+export default loading;
