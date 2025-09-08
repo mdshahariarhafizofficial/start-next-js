251e40b50d1356648612cabc9a08be80ccb53299
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-08 19:49:54 +0600

    Custom Css added

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import style from "../post.module.css"
 export const fetchSinglePost = async (id) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
   const data = res.json();
@@ -14,7 +14,7 @@ const page = async ({ params }) => {
   return (
     <div className="p-5 bg-gray-300 rounded-2xl space-y-6 mx-40">
       <h4>{post.id}</h4>
-      <h2>{post.title}</h2>
+      <h2 className={`${style["title"]}`}>{post.title}</h2>
       <p>{post.body}</p>
     </div>
   );
