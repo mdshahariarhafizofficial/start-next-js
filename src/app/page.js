b64e570f91361cd4416b997cb973436f650bbd9f
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-10 21:53:30 +0600

    Faq Section Added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./Components/Hero";
 import Feature from "./Components/Feature";
 import Blogs from "./Components/Blogs";
+import Faq from "./Components/Faq";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero></Hero>
       <Feature></Feature>
       <Blogs></Blogs>
+      <Faq></Faq>
     </>
   );
 }
